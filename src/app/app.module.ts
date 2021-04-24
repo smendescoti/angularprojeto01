61144c4662aae5bfabdc5fa0d2fdc93d3d9d26cf
smendescoti <sergio.coti@gmail.com>
2021-04-24 09:11:26 -0300

    Aula 03 Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,29 @@ import { ConsultaClientesComponent } from './consulta-clientes/consulta-clientes
 //estas rotas serão utilizadas para navegação da SPA - Single Page Application
 import { Routes, RouterModule } from '@angular/router';
 
+//importando as classes para uso da biblioteca de formulários do angular
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+//importando as classes para uso da biblioteca de acesso à APIs do angular (Http Client)
+import { HttpClientModule } from '@angular/common/http';
+
+//importando o módulo para configuração da biblioteca NgxPagination
+import { NgxPaginationModule } from 'ngx-pagination';
+
+//importando o módulo para configuração do Ng2SearchFilter
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
+//importando o módulo para configuração do NgxMask
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+
 //mapear uma rota (URL) para cada componente do projeto..
 const routes : Routes = [
   { path : 'cadastro-clientes', component : CadastroClientesComponent },
   { path : 'consulta-clientes', component : ConsultaClientesComponent }
 ];
+
+//configuração para inicialização do NgxMask
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
 
 @NgModule({
   declarations: [
@@ -23,6 +41,12 @@ const routes : Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule, //registrando biblioteca para chamadas de API
+    FormsModule, //registrando biblioteca para formulários
+    ReactiveFormsModule, //registrando biblioteca para formulários
+    NgxPaginationModule, //registrando biblioteca NgxPagination
+    Ng2SearchPipeModule, //registrando biblioteca Ng2SearchFilter
+    NgxMaskModule.forRoot(), //registrando biblioteca NgxMask
     RouterModule.forRoot(routes) //registrando a configuração de rotas!
   ],
   providers: [],
--- src/app/consulta-clientes/consulta-clientes.component.ts
+++ src/app/consulta-clientes/consulta-clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientesService } from '../services/clientes.service';
 
 @Component({
   selector: 'app-consulta-clientes',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultaClientesComponent implements OnInit {
 
-  constructor() { }
+  // atributos (campos)
+  clientes = []; // JSON array
 
-  ngOnInit(): void {
+  // armazenar o numero da página que está sendo
+  // acessada no componente de paginação
+  page = 1;
+
+  // atributo para armazenar o filtro de pesquisa
+  filtro: string;
+
+  //atributo para armazenar os dados do cliente
+  cliente = {
+    idCliente : 0, nome: '', email : '', cpf : ''
   }
 
+  // inicializando o ClientesService por meio de injeção de dependência
+  constructor(private clientesService: ClientesService) { }
+
+  // função executada sempre que o componente é renderizado
+  ngOnInit(): void {
+    // executar a consulta de clientes
+    this.consultarClientes();
+  }
+
+  // função para executar a consulta de clientes na API
+  consultarClientes(): void {
+
+    this.clientesService.getAll()
+      .subscribe( // promisse da API
+        (data: any[]) => { // obtendo o retorno de sucesso da API..
+          this.clientes = data;
+        },
+        e => { // obtendo o retorno de erro da API..
+          console.log(e);
+        }
+      );
+  }
+
+  obterCliente(idCliente): void {
+
+    this.clientesService.getById(idCliente)
+      .subscribe(
+        (data:any) => {
+          this.cliente = data;
+        },
+        e => {
+          console.log(e);
+        }
+      );
+  }
+
+  // função para realizar a paginação no
+  // componente ngx-pagination
+  handlePageChange(event): void {
+    this.page = event;
+  }
 }
